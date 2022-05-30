c10fd7f4d03486f35d817efe895c3450ce72e24e
MaximilianoBonnet <maxbonn@hotmail.com>
2022-05-29 21:23:55 -0300

    changes in transfer services

---
+++ src/routes/transfer/internal-to-external/detail.ts
@@ -0,0 +1,55 @@
+import { FastifyInstance } from "fastify";
+import { StrictResource } from "fastify-autoroutes";
+import { Static, Type } from "@sinclair/typebox";
+import Boom from "@hapi/boom";
+import { SWAGGER_TAGS } from "../../server/tags";
+import { transferDetails } from "../../modules/services/transfer/transferService";
+
+const RequestParamsSchema = Type.Object({
+  id: Type.String(),
+});
+
+const ResponseSchema = Type.Object({});
+
+type RequestParamsType = Static<typeof RequestParamsSchema>;
+
+export default (_server: FastifyInstance): StrictResource => ({
+  post: {
+    schema: {
+      body: RequestParamsSchema,
+      tags: [SWAGGER_TAGS.TRANSFER],
+      response: {
+        200: {
+          ...ResponseSchema,
+          description: "Successful request",
+        },
+        500: {
+          type: "object",
+          properties: {
+            statusCode: { type: "number" },
+            error: { type: "string" },
+            message: { type: "string" },
+          },
+          description: "Internal Server Error",
+        },
+      },
+    },
+    handler: async (request, reply) => {
+      try {
+        const config = request.body as RequestParamsType;
+        const response = await transferDetails.getDetails(config);
+        reply.status(200).send(response);
+      } catch (error) {
+        if (Boom.isBoom(error)) {
+          reply.status(error.output.statusCode).send(error.output.payload);
+        } else {
+          reply.status(500).send({
+            statusCode: 500,
+            error: error.name,
+            message: error.message,
+          });
+        }
+      }
+    },
+  },
+});
