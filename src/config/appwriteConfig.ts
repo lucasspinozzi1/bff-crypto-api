37133f5536e5da6b44680478f158590443fdf7a8
tomasgaute <tomas.gaute@omni.cr>
2022-06-02 20:55:52 -0400

    Logic to consulte Account Balance

--- src/config/appwriteConfig.ts
+++ src/config/appwriteConfig.ts
@@ -1,6 +1,6 @@
 import { API_KEY_GOD_MODE } from "./const";
 export const APPWRITE_CONFIG = {
-  endpoint: process.env.APPWRITE_ENDPOINT ?? "http://212e-186-12-184-169.ngrok.io/v1",
+  endpoint: process.env.APPWRITE_ENDPOINT ?? "http://3366-2803-9800-9886-48b5-eb-9797-f1e7-598e.ngrok.io/v1",
   project: process.env.APPWRITE_PROJECT ?? "62868821688a84ff78ee",
   key: API_KEY_GOD_MODE,
 };
--- src/modules/services/account/accountService.ts
+++ src/modules/services/account/accountService.ts
@@ -1,7 +1,8 @@
 import environment from "../../../config/env";
-import { IAccountCreate, IAccountDetails } from "./accountTypes";
+import { IAccountCreate, IAccountDetails, IAccountList } from "./accountTypes";
 import Account from "./create/create";
 import AccountDetails from "./detail/detail";
+import AccountList from "./list/list";
 
 export const ACCOUNT_CREATE: { [id: string]: IAccountCreate } = {
     A: new Account()
@@ -11,5 +12,10 @@ export const ACCOUNT_DETAILS: { [id: string]: IAccountDetails } = {
     A: new AccountDetails()
   };
 
+  export const ACCOUNT_LIST: { [id: string]: IAccountList } = {
+    A: new AccountList()
+  };
+
   export const accountService = ACCOUNT_CREATE[environment.service];
   export const accountDetails = ACCOUNT_DETAILS[environment.service];
+  export const accountList = ACCOUNT_LIST[environment.service];
